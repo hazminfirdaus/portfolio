0adc6a7059d5f8c4d369bec2366404e7ae1796fa
hazminfirdaus <cmhazminfirdaus@gmail.com>
2025-08-10 13:44:18 +0200

    created config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'smtp_host' => 'mail.hazminchik.com',
+    'smtp_port' => 587,
+    'smtp_username' => 'contact@hazminchik.com',
+    'smtp_password' => 'YOUR_EMAIL_PASSWORD',
+    'recipient_email' => 'contact@hazminchik.com'
+];
