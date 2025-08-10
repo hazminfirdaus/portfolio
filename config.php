88fd15b03d83636b85f15f994a1808e05fff4469
hazminfirdaus <cmhazminfirdaus@gmail.com>
2025-08-10 13:50:15 +0200

    update

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'smtp_host' => 'mail.hazminchik.com',
-    'smtp_port' => 587,
-    'smtp_username' => 'contact@hazminchik.com',
-    'smtp_password' => 'YOUR_EMAIL_PASSWORD',
-    'recipient_email' => 'contact@hazminchik.com'
-];
